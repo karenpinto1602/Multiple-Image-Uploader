b398b279f6e7862fd244d0e66d2d4b9bde61f707
Karen Pinto <karenpinto1602@gmail.com>
2022-01-30 03:27:46 +0530

    error code and upload and storage of images added

--- server/controller/controller.js
+++ server/controller/controller.js
@@ -2,3 +2,15 @@
 exports.home = (req,res) =>{
     res.render('main')
 }
+
+exports.uploads = (req,res,next) => {
+    const files = req.files;
+
+    if(!files){
+        const error = new Error("Please choose files");
+        error.httpStatusCode = 400;
+        return next(error)
+    }
+
+    res.json(files);
+}
